f4862f3049a23f35b4374b856458f25580ffa704
Ilya-Roschin <roshhin-78@mail.ru>
2021-08-01 19:30:44 +0300

    add task 2

---
+++ src/com/training/java/exercise/lesson2/lecture/LessonTwoTaskTwo.java
@@ -0,0 +1,17 @@
+package com.training.java.exercise.lesson2.lecture;
+
+public class LessonTwoTaskTwo {
+
+    public static void main(String[] args) {
+
+        int numb = 8;
+
+        if (numb % 2 == 0) {
+            System.out.println("even");
+        } else if (numb == 0) {
+            System.out.println("zero");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
