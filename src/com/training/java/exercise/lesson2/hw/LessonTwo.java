dd73ae42f5062fce425d661fd129dbeb961fcb3d
Ilya-Roschin <roshhin-78@mail.ru>
2021-08-02 11:49:28 +0300

    add task 1

--- src/com/training/java/exercise/lesson2/hw/LessonTwo.java
+++ src/com/training/java/exercise/lesson2/hw/LessonTwo.java
@@ -1,4 +1,22 @@
 package com.training.java.exercise.lesson2.hw;
 
 public class LessonTwo {
+
+    public static void main(String[] args) {
+
+        int a = 60;
+        int b = 30;
+
+        if (a + b < 180){
+            System.out.print("triangle exist ");
+
+            if (a+b == 90){
+                System.out.println("and it is rectangular");
+            }
+
+        } else {
+            System.out.println("triangle not exist ");
+        }
+
+    }
 }
