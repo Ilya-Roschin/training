de92fe0915e15c6690cc2242c39f6f681eb7c40b
Ilya-Roschin <roshhin-78@mail.ru>
2021-08-02 12:29:41 +0300

    add task 3

---
+++ src/com/training/java/exercise/lesson2/hw/LessonTwoTaskThree.java
@@ -0,0 +1,25 @@
+package com.training.java.exercise.lesson2.hw;
+
+public class LessonTwoTaskThree {
+
+    public static void main(String[] args) {
+
+        int x1 = -1;
+        int y1 = -3;
+
+        int x2 = 1;
+        int y2 = -1;
+
+        int x3 = 2;
+        int y3 = 0;
+
+        if ((x3 - x1) / (x2 - x1) == (y3 - y1) / (y2 - y1)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+
+        }
+
+
+    }
+}
