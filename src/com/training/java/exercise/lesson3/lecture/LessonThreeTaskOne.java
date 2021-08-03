a95ee6cfb08c01ff026405c20a73e7ce2510834e
Ilya-Roschin <roshhin-78@mail.ru>
2021-08-03 09:54:36 +0300

    add task 1 lessonThree

---
+++ src/com/training/java/exercise/lesson3/lecture/LessonThreeTaskOne.java
@@ -0,0 +1,36 @@
+package com.training.java.exercise.lesson3.lecture;
+
+import javax.sound.midi.Soundbank;
+import java.awt.*;
+
+public class LessonThreeTaskOne {
+
+    public static void main(String[] args) {
+
+        int[][] nums = {
+                {1, 2, 3, 0, 0},
+                {4, 5, 6, 1, 1},
+                {7, 8, 9, 5}
+        };
+
+//        for (int[] arr : nums) {
+//            for (int i : arr) {
+//                System.out.print(i + ", ");
+//            }
+//            System.out.println();
+//        }
+//        int[] numsNew = {1, 2, 3};
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = 0; j < nums[i].length; j++) {
+//                System.out.print(nums[i][j] + ", ");
+//            }
+//                System.out.println();
+//        }
+//
+
+
+
+
+    }
+}
