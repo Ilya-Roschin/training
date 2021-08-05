19761d0913b3229717bfc7e37571f4fd980eb915
Ilya-Roschin <roshhin-78@mail.ru>
2021-08-05 13:31:43 +0300

    add task 6

---
+++ src/com/training/java/exercise/lesson3/hw/LessonThreeTaskSix.java
@@ -0,0 +1,14 @@
+package com.training.java.exercise.lesson3.hw;
+
+public class LessonThreeTaskSix {
+
+    public static void main(String[] args) {
+
+        char ascii;
+
+        for (int i = 0; i <= 255; i++) {
+            ascii = (char) i;
+            System.out.println(i + " : " + ascii);
+        }
+    }
+}
