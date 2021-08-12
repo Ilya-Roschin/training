703f9e724438d4238812110577b09c8fa5b68367
Ilya-Roschin <roshhin-78@mail.ru>
2021-08-12 17:36:23 +0300

    add task 2

---
+++ src/com/training/java/exercise/lesson5/hw/LessonFiveTaskTwo.java
@@ -0,0 +1,23 @@
+package com.training.java.exercise.lesson5.hw;
+
+import java.util.Scanner;
+
+public class LessonFiveTaskTwo {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (number >= 60) {
+            System.out.println("Great");
+        } else if (number >= 30 && number < 60) {
+            System.out.println("Good");
+        } else if (number >= 17 && number < 30) {
+            System.out.println("satisfyingly");
+        } else {
+            System.out.println("bad");
+        }
+    }
+}
---
+++ src/com/training/java/exercise/lesson5/lecture/LessonFiveTaskTwo.java
@@ -0,0 +1,16 @@
+package com.training.java.exercise.lesson5.lecture;
+
+import java.util.Arrays;
+
+public class LessonFiveTaskTwo {
+
+    public static void main(String[] args) {
+
+        int[] array = {-7, 2, 1};
+
+        Arrays.sort(array);
+
+        System.out.println(array[array.length - 1]);
+
+    }
+}
