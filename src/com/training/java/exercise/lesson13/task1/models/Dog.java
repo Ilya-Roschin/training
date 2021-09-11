04845fa2de263b8b1ebfe78d1232c69ea7b73e96
Ilya-Roschin <roshhin-78@mail.ru>
2021-09-11 15:21:00 +0300

    add lesson 12 task one

---
+++ src/com/training/java/exercise/lesson13/task1/models/Dog.java
@@ -0,0 +1,25 @@
+package com.training.java.exercise.lesson13.task1.models;
+
+public class Dog extends Animal {
+
+    public static final String ANIMAL_NAME = "Dog";
+
+    public Dog(int food, int location) {
+        super(food, location);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Gav");
+    }
+
+    @Override
+    public void eat() {
+        food += 30;
+    }
+
+    @Override
+    public String getAnimalName() {
+        return ANIMAL_NAME;
+    }
+}
