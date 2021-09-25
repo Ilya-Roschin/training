84d2b899ece4a4a2af30f11f4f244a2994c5bc43
Ilya-Roschin <roshhin-78@mail.ru>
2021-09-25 16:46:47 +0300

    refactor lesson 14

--- src/com/training/java/exercise/lesson14/lowLevel/TaskThree.java
+++ src/com/training/java/exercise/lesson14/lowLevel/TaskThree.java
@@ -1,24 +1,20 @@
 package com.training.java.exercise.lesson14.lowLevel;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskThree {
 
     public int amountOfNumbers(String userString) {
         String[] strings = userString.split(" ");
         int result = 0;
-        for (int i = 0; i < strings.length; i++) {
-            boolean check = true;
-            int j = 0;
-            while (j < strings[i].length()) {
-                if (strings[i].charAt(j) <= 48 || strings[i].charAt(j) >= 57) {
-                    check = false;
-                    break;
-                }
-                j++;
-            }
-            if (check) {
-                result++;
-            }
+        Pattern pattern = Pattern.compile("\\d{3,}");
+        Matcher matcher = pattern.matcher(userString);
+        while (matcher.find()) {
+            result++;
         }
         return result;
     }
 }
+
+// TODO: 16.09.2021 https://www.google.com/search?q=regular+expression+java&sxsrf=AOaemvIoQyRv1p2BN-WlID2YnpDEZ1spvw%3A1631772211388&ei=M95CYf3qFv2L9u8P2Nag2AQ&oq=regular+expression+java&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEMsBMgUIABCABDIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBOgcIIxCwAxAnOgcIABBHELADOgcIABCwAxBDOgQIIxAnOgQIABBDOgQIABANSgQIQRgAUOIJWMAVYP0WaAJwAngAgAFuiAGlBJIBAzQuMpgBAKABAcgBCsABAQ&sclient=gws-wiz&ved=0ahUKEwi90I2Y6YLzAhX9hf0HHVgrCEsQ4dUDCA4&uact=5
--- src/com/training/java/exercise/lesson14/lowLevel/TaskTwo.java
+++ src/com/training/java/exercise/lesson14/lowLevel/TaskTwo.java
@@ -1,14 +1,19 @@
 package com.training.java.exercise.lesson14.lowLevel;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskTwo {
 
     public int numberOfDigits(String userString) {
         int result = 0;
-        for (int i = 0; i < userString.length(); i++) {
-            if (userString.charAt(i) >= 48 && userString.charAt(i) <= 57) {
-                result++;
-            }
+        Pattern pattern = Pattern.compile("\\d");
+        Matcher matcher = pattern.matcher(userString);
+        while (matcher.find()) {
+            result++;
         }
         return result;
     }
 }
+
+// TODO: 16.09.2021 переделать с регулярными выражениями
